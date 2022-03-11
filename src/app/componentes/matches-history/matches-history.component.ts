d416479cb90994434f082b3fb074fdf202505015
Diego Veintimilla Portilla <63263180+dmveintimilla@users.noreply.github.com>
2022-03-11 11:53:48 -0500

    maquetacion pags principales
    maquetacion

---
+++ src/app/componentes/matches-history/matches-history.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-matches-history',
+  templateUrl: './matches-history.component.html',
+  styleUrls: ['./matches-history.component.scss'],
+})
+export class MatchesHistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ src/app/componentes/matches-info/matches-info.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-matches-info',
+  templateUrl: './matches-info.component.html',
+  styleUrls: ['./matches-info.component.scss'],
+})
+export class MatchesInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
