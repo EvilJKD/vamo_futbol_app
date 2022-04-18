97b5e9bb5fa9cd85c8bea92b2af3a1a08dca3c14
EvilJKD <jmcadenaz@gmail.com>
2022-04-18 16:49:13 -0500

    Personalizacion Fechas Tab1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -5,7 +5,7 @@ import { DataService } from '../services/data.service';
 
 // PLUGIN
 import { Geolocation } from '@awesome-cordova-plugins/geolocation/ngx';
-import {distanceBetweenTwoPoints} from '../helpers/utils'
+import { distanceBetweenTwoPoints } from '../helpers/utils'
 
 @Component({
   selector: 'app-tab1',
@@ -15,28 +15,35 @@ import {distanceBetweenTwoPoints} from '../helpers/utils'
 export class Tab1Page {
 
   currentUser: any;
-
+  rangeOfDates: any[];
   matches;
 
   constructor(private dataService: DataService,
     private geolocation: Geolocation) { }
 
   async ngOnInit() {
-    const {coords:{latitude, longitude}} = await this.geolocation.getCurrentPosition();
+	//Rango de fechas para seleccionar
+	const today = new Date();
+	const afterOneWeek = new Date();
+	afterOneWeek.setDate(afterOneWeek.getDate()+7);
+	this.rangeOfDates = this.getRangeOfDates(today, afterOneWeek);
+
+
+    const { coords: { latitude, longitude } } = await this.geolocation.getCurrentPosition();
 
     this.dataService.getNearbyFields(latitude, longitude, 50).subscribe(res => {
       const fieldsIds = res.map(field => {
         const id = field.id
         return id;
       })
-      
-      if(fieldsIds.length == 0){
+
+      if (fieldsIds.length == 0) {
         console.log('No hay canchas cercanas')
         this.matches = []
-        return 
+        return
       }
       this.dataService.getMatchesByFields(fieldsIds).subscribe((matches) => {
-       this.matches = this.populateMatches(matches, {lat: latitude, long: longitude})
+        this.matches = this.populateMatches(matches, { lat: latitude, long: longitude })
       });
     });
 
@@ -52,20 +59,20 @@ export class Tab1Page {
     matchesArr.forEach((match) => {
       //Field Info Population
       this.dataService.getFieldById(match.match_field).subscribe((fieldInfo) => {
-        const {field_name, latitud, longitud, max_players, status} = fieldInfo;
+        const { field_name, latitud, longitud, max_players, status } = fieldInfo;
         const posicionCancha = {
-          lat: latitud, 
+          lat: latitud,
           long: longitud
         }
         const distance = `${distanceBetweenTwoPoints(posicion, posicionCancha)} km`
 
-        match.match_field= {field_name, distance, max_players, status};
+        match.match_field = { field_name, distance, max_players, status };
       })
 
       match.match_players.forEach((player, idx) => {
         this.dataService.getUserById(player).subscribe((playerInfo) => {
-          const {first_name, last_name, phone_number, picture_url} = playerInfo;
-          match.match_players[idx] = {first_name, last_name, phone_number, picture_url};
+          const { first_name, last_name, phone_number, picture_url } = playerInfo;
+          match.match_players[idx] = { first_name, last_name, phone_number, picture_url };
         })
       })
     })
@@ -73,4 +80,33 @@ export class Tab1Page {
     return matchesArr;
   }
 
+
+  getRangeOfDates(startDate, endDate) {
+    let dates = []
+	let weekdays = {
+		0: 'Dom.', 
+		1: 'Lun.', 
+		2: 'Mart.', 
+		3: 'Miérc.',
+		4: 'Juev.', 
+		5: 'Vier.', 
+		6: 'Sáb.'
+	}
+
+    const theDate = startDate
+    while (theDate < endDate) {
+      dates = [...dates, 
+		{
+			day: theDate.getDate(),
+			daySt: weekdays[theDate.getDay()],
+			month: theDate.getMonth()+1,
+			year: theDate.getFullYear()
+		}]
+      theDate.setDate(theDate.getDate() + 1)
+    }
+
+	console.log('Fechas', dates)
+    return dates
+
+  }
 }
