5f8b4621315c55bed51587a7250efb7e2c48315b
Diego Veintimilla Portilla <63263180+dmveintimilla@users.noreply.github.com>
2022-04-26 23:42:43 -0500

    validation signup page
    Validacion de todos los campos de signup con propuesto para añadir más campos como num tlf etc

--- src/app/pages/signup/signup.module.ts
+++ src/app/pages/signup/signup.module.ts
@@ -1,19 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { SignupPageRoutingModule } from './signup-routing.module';
 
 import { SignupPage } from './signup.page';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: SignupPage
+  }
+];
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    SignupPageRoutingModule
+    ReactiveFormsModule,
+    RouterModule.forChild(routes)
   ],
   declarations: [SignupPage]
 })
--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { UsernameValidator } from '../validators/username.validator';
+import { PhoneValidator } from '../validators/phone.validator';
+import { PasswordValidator } from '../validators/password.validator';
+import { CountryPhone } from './country-phone.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -8,13 +14,118 @@ import { Location } from '@angular/common';
 })
 export class SignupPage implements OnInit {
 
-  constructor(private location: Location) { }
+  validations_form: FormGroup;
+  matching_passwords_group: FormGroup;
+  country_phone_group: FormGroup;
+
+  countries: Array<CountryPhone>;
+  genders: Array<string>;
+
+  constructor(private location: Location,
+    public formBuilder: FormBuilder,
+    private router: Router) { }
 
   backButtonClick(){
     this.location.back();
   }
 
   ngOnInit() {
+    //  We just use a few random countries, however, you can use the countries you need by just adding them to this list.
+    // also you can use a library to get all the countries from the world.
+    this.countries = [
+      new CountryPhone('UY', 'Uruguay'),
+      new CountryPhone('US', 'United States'),
+      new CountryPhone('BR', 'Brasil')
+    ];
+
+    this.genders = [
+      "Male",
+      "Female"
+    ];
+
+    this.matching_passwords_group = new FormGroup({
+      password: new FormControl('', Validators.compose([
+        Validators.minLength(5),
+        Validators.required,
+        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
+      ])),
+      confirm_password: new FormControl('', Validators.required)
+    }, (formGroup: FormGroup) => {
+      return PasswordValidator.areEqual(formGroup);
+    });
+
+    let country = new FormControl(this.countries[0], Validators.required);
+    let phone = new FormControl('', Validators.compose([
+      Validators.required,
+      PhoneValidator.validCountryPhone(country)
+    ]));
+    this.country_phone_group = new FormGroup({
+      country: country,
+      phone: phone
+    });
+
+    this.validations_form = this.formBuilder.group({
+      username: new FormControl('', Validators.compose([
+        UsernameValidator.validUsername,
+        Validators.maxLength(25),
+        Validators.minLength(5),
+        Validators.pattern('^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$'),
+        Validators.required
+      ])),
+      name: new FormControl('', Validators.required),
+      lastname: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      gender: new FormControl(this.genders[0], Validators.required),
+      country_phone: this.country_phone_group,
+      matching_passwords: this.matching_passwords_group,
+      terms: new FormControl(true, Validators.pattern('true'))
+    });
+  }
+
+  validation_messages = {
+    'username': [
+      { type: 'required', message: 'Username is required.' },
+      { type: 'minlength', message: 'Username must be at least 5 characters long.' },
+      { type: 'maxlength', message: 'Username cannot be more than 25 characters long.' },
+      { type: 'pattern', message: 'Your username must contain only numbers and letters.' },
+      { type: 'validUsername', message: 'Your username has already been taken.' }
+    ],
+    'name': [
+      { type: 'required', message: 'Ingrese su nombre.' }
+    ],
+    'lastname': [
+      { type: 'required', message: 'Last name is required.' }
+    ],
+    'email': [
+      { type: 'required', message: 'Ingrese su email.' },
+      { type: 'pattern', message: 'Por favor, ingrese un email valido.' }
+    ],
+    'phone': [
+      { type: 'required', message: 'Phone is required.' },
+      { type: 'validCountryPhone', message: 'The phone is incorrect for the selected country.' }
+    ],
+    'password': [
+      { type: 'required', message: 'Ingrese su contraseña.' },
+      { type: 'minlength', message: 'La contraseña debe tener al menos 5 caracteres.' },
+      { type: 'pattern', message: 'Su contraseña debe contener al menos una mayúscula, una minúscula y un número.' }
+    ],
+    'confirm_password': [
+      { type: 'required', message: 'Ingrese la contraseña de confirmación.' }
+    ],
+    'matching_passwords': [
+      { type: 'areEqual', message: 'Las constraseñas no coinciden.' }
+    ],
+    'terms': [
+      { type: 'pattern', message: 'You must accept terms and conditions.' }
+    ],
+  };
+
+  onSubmit(values){
+    console.log(values);
+    this.router.navigate(["/user"]);
   }
 
 }
